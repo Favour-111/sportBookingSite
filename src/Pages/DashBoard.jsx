ac85d3820bda16cb06f79710a0b7c6ce23feaa3a
favour <omojolaobaloluwa@gmail.com>
2025-10-25 21:28:31 +0100

    done

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -48,6 +48,7 @@ const DashBoard = () => {
   const [page, setPage] = useState("live");
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
+  const [openModal2, setOpenModal2] = useState(false);
   const [items, setItems] = useState(false);
   const [imageModal, setImageModal] = useState(null);
   const [ViewOpen, setViewOpen] = useState(false);
@@ -666,7 +667,7 @@ const DashBoard = () => {
                                   <div className="flex items-center gap-2">
                                     <button
                                       onClick={() => {
-                                        setOpenModal(true);
+                                        setOpenModal2(true);
                                         setItems(item);
                                       }}
                                       className="rounded-full duration-200  hover:opacity-80 bg-[#f6f6f6] dark:bg-[var(--default)] h-10 w-10 flex items-center justify-center"
@@ -868,6 +869,122 @@ const DashBoard = () => {
                       type="button"
                       onClick={() => {
                         setOpenModal(false);
+                        setTimeOut(() => {
+                          setItems(null); // Close the modal
+                        }, 200);
+                      }}
+                      className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto"
+                    >
+                      close
+                    </button>
+                  </div>
+                </DialogPanel>
+              </div>
+            </div>
+          </Dialog>
+          <Dialog
+            open={openModal2}
+            items={items}
+            onClose={setOpenModal2}
+            className="relative z-10"
+          >
+            <DialogBackdrop
+              transition
+              className="fixed inset-0 bg-gray-500/75 transition-opacity"
+            />
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:size-10">
+                        <BsInfo className="size-6 text-blue-600" />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <DialogTitle className="text-base font-semibold text-gray-900">
+                          {items?.tipName}
+                        </DialogTitle>
+
+                        {/* Display relevant details */}
+                        <div className="mt-2 flex items-center gap-1">
+                          <p className="text-sm font-[600] text-gray-500">
+                            Tip Price:{" "}
+                          </p>
+                          <p className="text-sm text-gray-500">
+                            ${items?.tipPrice}
+                          </p>
+                        </div>
+                        <div className="mt-2 flex items-center gap-1">
+                          <p className="text-sm font-[600] text-gray-500">
+                            Odd Ratio:{" "}
+                          </p>
+                          <p className="text-sm text-gray-500">
+                            {items?.oddRatio}
+                          </p>
+                        </div>
+                        <div className="mt-2 flex items-center gap-1">
+                          <p className="text-sm font-[600] text-gray-500">
+                            Game Status :
+                          </p>
+                          {items?.status === "Pending" ? (
+                            <p className="text-sm bg-[#f6f6f6] px-4 py-1 text-sm rounded-full text-gray-500">
+                              ⏳ Pending
+                            </p>
+                          ) : items?.status === "Hit✅" ? (
+                            <p className=" bg-green-100 text-green-600 px-4 py-1 text-sm rounded-full ">
+                              ✅ Game Won
+                            </p>
+                          ) : (
+                            <p className=" bg-red-100 text-red-600 px-4 py-1 text-sm rounded-full ">
+                              ❌ Game Lost
+                            </p>
+                          )}
+                        </div>
+
+                        <div className="mt-2 flex items-center gap-1">
+                          <p className="text-sm font-[600] text-gray-500">
+                            Full Content:{" "}
+                          </p>
+                          <p className="text-sm font-[700] text-green-600">
+                            {items?.contentAfterPurchase}
+                          </p>
+                        </div>
+
+                        {/* Other important instructions */}
+                        <p className="mt-3 text-sm font-[600] text-gray-500 whitespace-nowrap">
+                          ⚠ Important instructions:
+                        </p>
+                        <div className="mt-2 flex items-center gap-1">
+                          <p>🎲 Place bet on betting site listed above</p>
+                        </div>
+                        <div className="mt-2 flex items-center gap-1">
+                          <p>🔒 This content is for your use only</p>
+                        </div>
+
+                        <div className="mt-2 flex items-center gap-1">
+                          <p>🚫 Do not share content with others</p>
+                        </div>
+                        <div className="mt-2 flex items-center gap-1">
+                          <p>
+                            📞 For any issues or questions, please click the
+                            support button to contact us{" "}
+                            <a
+                              href=""
+                              className="text-sm font-[700] text-blue-700 underline"
+                            >
+                              Support
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setOpenModal2(false);
                         setTimeOut(() => {
                           setItems(null); // Close the modal
                         }, 200);
